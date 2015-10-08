7b1c382350077e66473239ee231ec194b771b199
Chris Johnson <chris@johnsonch.com>
2015-10-08 01:20:35 +0000

    end of class

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'static_pages/index'
 
   resources :tasks
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924230340) do
+ActiveRecord::Schema.define(version: 20151001224335) do
 
   create_table "clients", force: :cascade do |t|
     t.string   "name"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 20150924230340) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "project_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
