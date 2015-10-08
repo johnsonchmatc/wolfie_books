d0dad21e9455c139994b331d9a9cc00af81ae329
Chris Johnson <chris@johnsonch.com>
2015-10-08 16:00:49 -0500

    week 6 prep work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001224335) do
+ActiveRecord::Schema.define(version: 20151008013839) do
 
   create_table "clients", force: :cascade do |t|
     t.string   "name"
@@ -49,8 +49,9 @@ ActiveRecord::Schema.define(version: 20151001224335) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
