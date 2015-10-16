6aa6121d647deb9c50f33df56be3c5322c74011c
Chris Johnson <chris@johnsonch.com>
2015-10-16 13:22:46 +0000

    what we did in class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-	flash[:success] = "Welcome to Wolfie Books"
-	redirect_to @user
+	    flash[:success] = "Welcome to Wolfie Books"
+	    log_in(@user)
+	    redirect_to @user
     else
       render 'new'
     end
